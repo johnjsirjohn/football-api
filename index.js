7bf18996f1aebfe2846228a335edea0b46b4f3ef
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-26 13:47:32 +0200

    Adding a start script in the package.json file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log('Express API listening on port 4000'));
+app.listen(port, () => console.log(`Express API listening on port ${port}`));
+
+app.get('/', (request, response) => {
+  response.send(`I'll do something, I promise!${port}`);
+});
