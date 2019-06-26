5c4a965e87a80dee6afa0493ad58585e542be0f0
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-26 15:00:45 +0200

    Creating db.js file running on localhost

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres';
+const sequelize = new Sequelize(databaseUrl);
+
+sequelize
+  .sync()
+  .then(() => console.log('Database schema updated'))
+  .catch(console.error);
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const Sequelize = require('./db');
+
 const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Express API listening on port ${port}`));
+app.listen(port, () => console.log(`Express API listening on port: ${port}`));
 
 app.get('/', (request, response) => {
-  response.send(`I'll do something, I promise!${port}`);
+  response.send(`I'll do something, I promise!!${port}`);
 });
